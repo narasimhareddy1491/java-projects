ae2d74a2ef96a9a8ff2d2ba7d03218abdbda95fe
narasimhareddy1491 <simha1491@gmail.com>
2024-11-15 20:34:43 -0500

    Add files via upload

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"receipe","l":"Ingredient"},{"p":"receipe","l":"Recipe"},{"p":"icecreams","l":"RecipeBox"}];updateSearchResults();
